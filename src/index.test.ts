eef658cbb2943bfdf54947bb94523431d806641c
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-09 13:47:58 +0000

    Write tests for board states
    Colours abbreviated to letters only - full word not necessary, a little
    verbose.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { Chessboard } from './index';
+import { Colour, Square } from './types';
+import { start } from 'repl';
 
 describe('Chessboard instance', () => {
     it('Instantiates', () => {
@@ -14,12 +16,59 @@ describe('Board', () => {
         expect(chessboard.board.every((row) => row.length === 8)).toBe(true);
     });
 
-    // TODO
-    it.skip('Starts with standard piece count', () => {});
+    it.skip('Starts with standard piece count', () => {
+        const piecesPerPlayer = { p: 8, N: 2, B: 2, R: 2, Q: 1, K: 1 };
+        const pieceCounts = {
+            w: piecesPerPlayer,
+            b: piecesPerPlayer,
+        };
 
-    // TODO
-    it.skip('Starts with standard piece placement', () => {});
+        const board = new Chessboard().board;
+        board.forEach((row) => {
+            row.forEach((square: Square) => {
+                if (square === null) return;
+                const piece = square;
+                pieceCounts[piece.colour][piece.letter]--;
+            });
+        });
 
-    // TODO
-    it.skip('Can flip board orientation', () => {});
+        function checkPieceCount(colour: Colour) {
+            return Object.values(pieceCounts[colour]).every(
+                (count) => count === 0
+            );
+        }
+        expect(checkPieceCount('w')).toBe(true);
+        expect(checkPieceCount('b')).toBe(true);
+    });
+
+    it.skip('Starts with standard piece placement', () => {
+        const startingBoard = [
+            ['R-b', 'N-b', 'B-b', 'Q-b', 'K-b', 'B-b', 'N-b', 'R-b'],
+            ['P-b', 'P-b', 'P-b', 'P-b', 'P-b', 'P-b', 'P-b', 'P-b'],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            ['P-w', 'P-w', 'P-w', 'P-w', 'P-w', 'P-w', 'P-w', 'P-w'],
+            ['R-w', 'N-w', 'B-w', 'Q-w', 'K-w', 'B-w', 'N-w', 'R-w'],
+        ];
+        const actualBoard = new Chessboard().board.map((row) =>
+            row.map(
+                (square: Square) =>
+                    square && `${square.letter}-${square.colour}`
+            )
+        );
+
+        expect(actualBoard).toEqual(startingBoard);
+    });
+
+    it.skip('Can flip board orientation', () => {
+        const board = new Chessboard().board;
+        const whiteKing = board[7][4];
+        const blackLightBishop = board[0][2];
+
+        board.flip();
+        expect(whiteKing).toBe(board[0][3]);
+        expect(blackLightBishop).toBe(board[7][5]);
+    });
 });
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type Colour = 'white' | 'black';
+export type Colour = 'w' | 'b';
 
 type CoordinateNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 export type Coordinate = [CoordinateNumber, CoordinateNumber];
