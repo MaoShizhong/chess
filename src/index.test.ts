eb3c94d85f224286ee8e15e18ff06d0922f1550f
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-10 23:10:13 +0000

    Add tests for new chessboard players

--- src/index.test.ts
+++ src/index.test.ts
@@ -114,3 +114,29 @@ describe('Board', () => {
         expect(chessboard.board[0][4]?.letter).toBe('k');
     });
 });
+
+describe('Players', () => {
+    it('Starts with 1 white and 1 black player', () => {
+        const chessboard = new Chessboard();
+        expect(Object.values(chessboard.players).length).toBe(2);
+        expect(chessboard.players.w.colour).toBe('w');
+        expect(chessboard.players.b.colour).toBe('b');
+    });
+
+    it("Starts on white's turn if no FEN passed in", () => {
+        const chessboard = new Chessboard();
+        expect(chessboard.activePlayer).toBe(chessboard.players.w);
+    });
+
+    it('Uses FEN to determine active player', () => {
+        const chessboard = new Chessboard(
+            'r1b1kb1r/ppp2ppp/3q4/8/P2Q4/8/1PP2PPP/RNB2RK1 w kq - 0 11'
+        );
+        expect(chessboard.activePlayer).toBe(chessboard.players.w);
+
+        const chessboard2 = new Chessboard(
+            'r1b1k1nr/pppp1ppp/2n5/2bNp3/2B1P1Q1/8/PPPP1qPP/R1BK2NR b kq - 1 6'
+        );
+        expect(chessboard2.activePlayer).toBe(chessboard2.players.b);
+    });
+});
