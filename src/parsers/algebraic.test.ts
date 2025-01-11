d718e5336db76951ad6426cd0afe2379a5d422a5
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-11 22:32:29 +0000

    Expect move array from parse
    Also changed castling destination rank from null to 0 due to typing
    simplicity. Null not necessary as it's just a placeholder to be changed
    in Player.prototype.move based on Player colour.

--- src/parsers/algebraic.test.ts
+++ src/parsers/algebraic.test.ts
@@ -5,48 +5,66 @@ import { RANK, FILE } from '../board/board';
 describe('Parsing algebraic notation', () => {
     describe('Non-captures', () => {
         it('Parses pawn moves (missing piece letter)', () => {
-            expect(algebraic.parse('a4')).toEqual({
-                piece: { letter: 'P' },
-                destination: [RANK[4], FILE.a],
-            });
-            expect(algebraic.parse('h6')).toEqual({
-                piece: { letter: 'P' },
-                destination: [RANK[6], FILE.h],
-            });
+            expect(algebraic.parse('a4')).toEqual([
+                {
+                    piece: { letter: 'P' },
+                    destination: [RANK[4], FILE.a],
+                },
+            ]);
+            expect(algebraic.parse('h6')).toEqual([
+                {
+                    piece: { letter: 'P' },
+                    destination: [RANK[6], FILE.h],
+                },
+            ]);
         });
 
         it('Parses unambiguous piece moves', () => {
-            expect(algebraic.parse('Ke2')).toEqual({
-                piece: { letter: 'K' },
-                destination: [RANK[2], FILE.e],
-            });
-            expect(algebraic.parse('Rh5')).toEqual({
-                piece: { letter: 'R' },
-                destination: [RANK[5], FILE.h],
-            });
-            expect(algebraic.parse('Bg7')).toEqual({
-                piece: { letter: 'B' },
-                destination: [RANK[7], FILE.g],
-            });
-            expect(algebraic.parse('Nc6')).toEqual({
-                piece: { letter: 'N' },
-                destination: [RANK[6], FILE.c],
-            });
+            expect(algebraic.parse('Ke2')).toEqual([
+                {
+                    piece: { letter: 'K' },
+                    destination: [RANK[2], FILE.e],
+                },
+            ]);
+            expect(algebraic.parse('Rh5')).toEqual([
+                {
+                    piece: { letter: 'R' },
+                    destination: [RANK[5], FILE.h],
+                },
+            ]);
+            expect(algebraic.parse('Bg7')).toEqual([
+                {
+                    piece: { letter: 'B' },
+                    destination: [RANK[7], FILE.g],
+                },
+            ]);
+            expect(algebraic.parse('Nc6')).toEqual([
+                {
+                    piece: { letter: 'N' },
+                    destination: [RANK[6], FILE.c],
+                },
+            ]);
         });
 
         it('Disambiguates ambiguous piece moves', () => {
-            expect(algebraic.parse('Nge2')).toEqual({
-                piece: { letter: 'N', file: FILE.g },
-                destination: [RANK[2], FILE.e],
-            });
-            expect(algebraic.parse('R2f3')).toEqual({
-                piece: { letter: 'R', rank: RANK[2] },
-                destination: [RANK[3], FILE.f],
-            });
-            expect(algebraic.parse('Qh1e1')).toEqual({
-                piece: { letter: 'Q', rank: RANK[1], file: FILE.h },
-                destination: [RANK[1], FILE.e],
-            });
+            expect(algebraic.parse('Nge2')).toEqual([
+                {
+                    piece: { letter: 'N', file: FILE.g },
+                    destination: [RANK[2], FILE.e],
+                },
+            ]);
+            expect(algebraic.parse('R2f3')).toEqual([
+                {
+                    piece: { letter: 'R', rank: RANK[2] },
+                    destination: [RANK[3], FILE.f],
+                },
+            ]);
+            expect(algebraic.parse('Qh1e1')).toEqual([
+                {
+                    piece: { letter: 'Q', rank: RANK[1], file: FILE.h },
+                    destination: [RANK[1], FILE.e],
+                },
+            ]);
         });
     });
 
@@ -55,21 +73,21 @@ describe('Parsing algebraic notation', () => {
             expect(algebraic.parse('O-O')).toEqual([
                 {
                     piece: { letter: 'K' },
-                    destination: [null, FILE.g],
+                    destination: [0, FILE.g],
                 },
                 {
                     piece: { letter: 'R', file: FILE.h },
-                    destination: [null, FILE.f],
+                    destination: [0, FILE.f],
                 },
             ]);
             expect(algebraic.parse('O-O-O')).toEqual([
                 {
                     piece: { letter: 'K' },
-                    destination: [null, FILE.g],
+                    destination: [0, FILE.c],
                 },
                 {
                     piece: { letter: 'R', file: FILE.a },
-                    destination: [null, FILE.f],
+                    destination: [0, FILE.d],
                 },
             ]);
         });
