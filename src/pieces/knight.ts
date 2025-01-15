17c0b9fe7ce32eec7673eaae880fc5e6cfa1646a
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-15 23:36:41 +0000

    Use getter for Knight maximum moves
    Forgot to change this in Knight when all other Pieces were changed.

--- src/pieces/knight.ts
+++ src/pieces/knight.ts
@@ -2,18 +2,20 @@ import { Colour, Moves } from '../types';
 import { Piece } from './piece';
 
 export class Knight extends Piece {
-    maximumMoves: Moves = [
-        [[1, 2]],
-        [[2, 1]],
-        [[2, -1]],
-        [[1, -2]],
-        [[-1, -2]],
-        [[-2, -1]],
-        [[-2, 1]],
-        [[-1, 2]],
-    ];
-
     constructor(colour: Colour) {
         super('N', colour);
+    }
+
+    get maximumMoves(): Moves {
+        return [
+            [[1, 2]],
+            [[2, 1]],
+            [[2, -1]],
+            [[1, -2]],
+            [[-1, -2]],
+            [[-2, -1]],
+            [[-2, 1]],
+            [[-1, 2]],
+        ];
     }
 }
