b7a97163223c50becf723c3513b9c0948c654443
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-16 00:02:49 +0000

    Amend move call details tests to check only from/to call details
    Players will determine if an algebraic move is valid and what pieces to move.\nThe board need only be told to move a piece from a source square to destination square.

--- src/players/player.test.ts
+++ src/players/player.test.ts
@@ -5,13 +5,13 @@ import { RANK, FILE } from '../board/board';
 beforeEach(vi.clearAllMocks);
 
 describe('Move', () => {
-    // https://lichess.org/analysis/standard/r2qkb1r/ppp2ppp/2n2n2/1B2pb2/3P1B2/2N2N2/PPP1Q1PP/R3K2R_w_KQkq_-_1_10
-    const chess = new Chess(
-        'r2qkb1r/ppp2ppp/2n2n2/1B2pb2/3P1B2/2N2N2/PPP1Q1PP/R3K2R w KQkq - 1 10'
-    );
-    chess.board.move = vi.fn();
-
     describe('Number of pieces moved', () => {
+        // https://lichess.org/analysis/standard/r2qkb1r/ppp2ppp/2n2n2/1B2pb2/3P1B2/2N2N2/PPP1Q1PP/R3K2R_w_KQkq_-_1_10
+        const chess = new Chess(
+            'r2qkb1r/ppp2ppp/2n2n2/1B2pb2/3P1B2/2N2N2/PPP1Q1PP/R3K2R w KQkq - 1 10'
+        );
+        chess.board.move = vi.fn();
+
         it('Calls chess.board.move once if piece is movable and not castling', () => {
             chess.players.w.move('a3');
             expect(chess.board.move).toHaveBeenCalledTimes(1);
@@ -32,70 +32,88 @@ describe('Move', () => {
         });
     });
 
-    describe('Call details (White)', () => {
-        test('a3 tells board to move a pawn to a3', () => {
+    describe('Valid move call details (White)', () => {
+        // https://lichess.org/analysis/standard/1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q_w_-_-_0_1
+        const chess = new Chess(
+            '1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q w - - 0 1'
+        );
+        chess.board.move = vi.fn();
+
+        test('a3 tells board to move piece on a2 to a3', () => {
             chess.players.w.move('a3');
             expect(chess.board.move).toHaveBeenCalledWith({
-                piece: { letter: 'P' },
-                destination: [RANK[3], FILE.a],
+                from: [RANK[2], FILE.a],
+                to: [RANK[3], FILE.a],
             });
         });
 
-        test('Bc4 tells board to move a bishop to c4', () => {
-            chess.players.w.move('Bc4');
+        test('a4 tells board to move piece on a2 to a4', () => {
+            chess.players.w.move('a4');
             expect(chess.board.move).toHaveBeenCalledWith({
-                piece: { letter: 'B' },
-                destination: [RANK[4], FILE.c],
+                from: [RANK[2], FILE.a],
+                to: [RANK[4], FILE.a],
             });
         });
 
-        test('Nge2 tells board to move a g-file knight to e2', () => {
-            chess.players.w.move('Nge2');
+        test('Bc4 tells board to move piece on c2 to b3', () => {
+            chess.players.w.move('Bb3');
             expect(chess.board.move).toHaveBeenCalledWith({
-                piece: { letter: 'N', file: FILE.g },
-                destination: [RANK[2], FILE.e],
+                from: [RANK[2], FILE.c],
+                to: [RANK[3], FILE.b],
             });
         });
 
-        test('Qh4e1 tells board to move an h-file 4th rank queen to e1', () => {
+        test('Ncd2 tells board to move piece on c4 to d2', () => {
+            chess.players.w.move('Ncd2');
+            expect(chess.board.move).toHaveBeenCalledWith({
+                from: [RANK[4], FILE.c],
+                to: [RANK[2], FILE.d],
+            });
+        });
+
+        test('Qh4e1 tells board to move piece on h4 to e1', () => {
             chess.players.w.move('Qh4e1');
             expect(chess.board.move).toHaveBeenCalledWith({
-                piece: { letter: 'Q', file: FILE.h, rank: RANK[4] },
-                destination: [RANK[1], FILE.e],
+                from: [RANK[4], FILE.h],
+                to: [RANK[1], FILE.e],
             });
         });
     });
 
-    describe('Call details (Black)', () => {
-        test('d5 tells board to move a pawn to d5', () => {
+    describe('Valid move call details (Black)', () => {
+        // https://lichess.org/analysis/standard/r3k2r/3p4/8/8/8/8/8/R3K3_w_Qkq_-_0_1
+        const chess = new Chess('r3k2r/3p4/8/8/8/8/8/R3K3 w Qkq - 0 1');
+        chess.board.move = vi.fn();
+
+        test('d5 tells board to move piece on d7 to d5', () => {
             chess.players.b.move('d5');
             expect(chess.board.move).toHaveBeenCalledWith({
-                piece: { letter: 'p' },
-                destination: [RANK[5], FILE.d],
+                from: [RANK[7], FILE.d],
+                to: [RANK[5], FILE.d],
             });
         });
 
-        test('O-O tells board to castle short', () => {
+        test('O-O tells board to move piece on e8 to g8, then on h8 to f8', () => {
             chess.players.b.move('O-O');
             expect(chess.board.move).toHaveBeenNthCalledWith(1, {
-                piece: { letter: 'k' },
-                destination: [RANK[8], FILE.g],
+                from: [RANK[8], FILE.e],
+                to: [RANK[8], FILE.g],
             });
             expect(chess.board.move).toHaveBeenNthCalledWith(2, {
-                piece: { letter: 'r', file: FILE.h },
-                destination: [RANK[8], FILE.f],
+                from: [RANK[8], FILE.h],
+                to: [RANK[8], FILE.f],
             });
         });
 
-        test('O-O-O tells board to castle short', () => {
+        test('O-O-O tells board to move piece on e8 to c8, then on a8 to d8', () => {
             chess.players.b.move('O-O-O');
             expect(chess.board.move).toHaveBeenNthCalledWith(1, {
-                piece: { letter: 'k' },
-                destination: [RANK[8], FILE.c],
+                from: [RANK[8], FILE.e],
+                to: [RANK[8], FILE.c],
             });
             expect(chess.board.move).toHaveBeenNthCalledWith(2, {
-                piece: { letter: 'r', file: FILE.a },
-                destination: [RANK[8], FILE.d],
+                from: [RANK[8], FILE.a],
+                to: [RANK[8], FILE.d],
             });
         });
     });
