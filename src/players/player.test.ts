1811f8fdf390a90b66dae461125e76e3b0c9a6bc
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-16 17:01:29 +0000

    Add tests for invalid player moves

--- src/players/player.test.ts
+++ src/players/player.test.ts
@@ -32,96 +32,120 @@ describe('Move', () => {
         });
     });
 
-    describe('Valid move call details (White)', () => {
-        // https://lichess.org/analysis/standard/1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q_w_-_-_0_1
-        const chess = new Chess(
-            '1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q w - - 0 1'
-        );
-        chess.board.move = vi.fn();
+    describe('Non-captures', () => {
+        describe('Valid move call details (White)', () => {
+            // https://lichess.org/analysis/standard/1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q_w_-_-_0_1
+            const chess = new Chess(
+                '1nkr2n1/2ppp3/8/2b5/2N1Q2Q/8/PPB5/1N1K3Q w - - 0 1'
+            );
+            chess.board.move = vi.fn();
 
-        test('a3 tells board to move piece on a2 to a3', () => {
-            chess.players.w.move('a3');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[2], FILE.a],
-                to: [RANK[3], FILE.a],
+            test('a3 tells board to move piece on a2 to a3', () => {
+                chess.players.w.move('a3');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[2], FILE.a],
+                    to: [RANK[3], FILE.a],
+                });
+            });
+
+            test('a4 tells board to move piece on a2 to a4', () => {
+                chess.players.w.move('a4');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[2], FILE.a],
+                    to: [RANK[4], FILE.a],
+                });
+            });
+
+            test('Bc4 tells board to move piece on c2 to b3', () => {
+                chess.players.w.move('Bb3');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[2], FILE.c],
+                    to: [RANK[3], FILE.b],
+                });
+            });
+
+            test('Ncd2 tells board to move piece on c4 to d2 (disambiguates by file)', () => {
+                chess.players.w.move('Ncd2');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[4], FILE.c],
+                    to: [RANK[2], FILE.d],
+                });
+            });
+
+            test('Q4h3 tells board to move piece on h4 to h3 (disambiguates by rank)', () => {
+                chess.players.w.move('Q4h3');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[4], FILE.h],
+                    to: [RANK[3], FILE.h],
+                });
+            });
+
+            test('Qh4e1 tells board to move piece on h4 to e1', () => {
+                chess.players.w.move('Qh4e1');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[4], FILE.h],
+                    to: [RANK[1], FILE.e],
+                });
             });
         });
 
-        test('a4 tells board to move piece on a2 to a4', () => {
-            chess.players.w.move('a4');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[2], FILE.a],
-                to: [RANK[4], FILE.a],
+        describe('Valid move call details (Black)', () => {
+            // https://lichess.org/analysis/standard/r3k2r/3p4/8/8/8/8/8/R3K3_w_Qkq_-_0_1
+            const chess = new Chess('r3k2r/3p4/8/8/8/8/8/R3K3 w Qkq - 0 1');
+            chess.board.move = vi.fn();
+
+            test('d5 tells board to move piece on d7 to d5', () => {
+                chess.players.b.move('d5');
+                expect(chess.board.move).toHaveBeenCalledWith({
+                    from: [RANK[7], FILE.d],
+                    to: [RANK[5], FILE.d],
+                });
+            });
+
+            test('O-O tells board to move piece on e8 to g8, then on h8 to f8', () => {
+                chess.players.b.move('O-O');
+                expect(chess.board.move).toHaveBeenNthCalledWith(1, {
+                    from: [RANK[8], FILE.e],
+                    to: [RANK[8], FILE.g],
+                });
+                expect(chess.board.move).toHaveBeenNthCalledWith(2, {
+                    from: [RANK[8], FILE.h],
+                    to: [RANK[8], FILE.f],
+                });
+            });
+
+            test('O-O-O tells board to move piece on e8 to c8, then on a8 to d8', () => {
+                chess.players.b.move('O-O-O');
+                expect(chess.board.move).toHaveBeenNthCalledWith(1, {
+                    from: [RANK[8], FILE.e],
+                    to: [RANK[8], FILE.c],
+                });
+                expect(chess.board.move).toHaveBeenNthCalledWith(2, {
+                    from: [RANK[8], FILE.a],
+                    to: [RANK[8], FILE.d],
+                });
             });
         });
 
-        test('Bc4 tells board to move piece on c2 to b3', () => {
-            chess.players.w.move('Bb3');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[2], FILE.c],
-                to: [RANK[3], FILE.b],
-            });
-        });
+        describe('Invalid moves', () => {
+            // https://lichess.org/analysis/standard/rnbqk2r/1pppppp1/4P2p/8/8/pP6/P1PP1PPP/R3KBNR_w_KQkq_-_0_5
+            const chess = new Chess(
+                'rnbqk2r/1pppppp1/4P2p/8/8/pP6/P1PP1PPP/R3KBNR w KQkq - 0 5'
+            );
+            chess.board.move = vi.fn();
 
-        test('Ncd2 tells board to move piece on c4 to d2 (disambiguates by file)', () => {
-            chess.players.w.move('Ncd2');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[4], FILE.c],
-                to: [RANK[2], FILE.d],
-            });
-        });
+            it('Does not call Board.prototype.move if no valid piece moves available', () => {
+                // destination blocked
+                chess.players.w.move('a3');
+                expect(chess.board.move).not.toHaveBeenCalled();
 
-        test('Q4h3 tells board to move piece on h4 to h3 (disambiguates by rank)', () => {
-            chess.players.w.move('Q4h3');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[4], FILE.h],
-                to: [RANK[3], FILE.h],
-            });
-        });
+                // piece doesn't exist
+                chess.players.w.move('Qc1');
+                expect(chess.board.move).not.toHaveBeenCalled();
 
-        test('Qh4e1 tells board to move piece on h4 to e1', () => {
-            chess.players.w.move('Qh4e1');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[4], FILE.h],
-                to: [RANK[1], FILE.e],
-            });
-        });
-    });
-
-    describe('Valid move call details (Black)', () => {
-        // https://lichess.org/analysis/standard/r3k2r/3p4/8/8/8/8/8/R3K3_w_Qkq_-_0_1
-        const chess = new Chess('r3k2r/3p4/8/8/8/8/8/R3K3 w Qkq - 0 1');
-        chess.board.move = vi.fn();
-
-        test('d5 tells board to move piece on d7 to d5', () => {
-            chess.players.b.move('d5');
-            expect(chess.board.move).toHaveBeenCalledWith({
-                from: [RANK[7], FILE.d],
-                to: [RANK[5], FILE.d],
-            });
-        });
-
-        test('O-O tells board to move piece on e8 to g8, then on h8 to f8', () => {
-            chess.players.b.move('O-O');
-            expect(chess.board.move).toHaveBeenNthCalledWith(1, {
-                from: [RANK[8], FILE.e],
-                to: [RANK[8], FILE.g],
-            });
-            expect(chess.board.move).toHaveBeenNthCalledWith(2, {
-                from: [RANK[8], FILE.h],
-                to: [RANK[8], FILE.f],
-            });
-        });
-
-        test('O-O-O tells board to move piece on e8 to c8, then on a8 to d8', () => {
-            chess.players.b.move('O-O-O');
-            expect(chess.board.move).toHaveBeenNthCalledWith(1, {
-                from: [RANK[8], FILE.e],
-                to: [RANK[8], FILE.c],
-            });
-            expect(chess.board.move).toHaveBeenNthCalledWith(2, {
-                from: [RANK[8], FILE.a],
-                to: [RANK[8], FILE.d],
+                // castling not available
+                chess.players.b.move('O-O-O');
+                expect(chess.board.move).not.toHaveBeenCalled();
             });
         });
     });
